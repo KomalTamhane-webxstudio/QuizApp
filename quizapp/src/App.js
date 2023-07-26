b54281c36fd7cf8a233dab05c3e63ec4a6823696
rushi <rushikakdeforever@gmail.com>
2023-07-26 22:17:37 +0530

    Initial test comment

--- quizapp/src/App.js
+++ quizapp/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import StudentResult from './Component/StudentResult';
 
 function App() {
   return (
     <div className="App">
-     
+     <StudentResult/>
     </div>
   );
 }
---
+++ quizapp/src/Component/StudentResult.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const StudentResult = () => {
+  return (
+    <div>
+      <h1 className='btn btn-primary'>This is student result page</h1>
+    </div>
+  )
+}
+
+export default StudentResult
